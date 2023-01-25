94723c33d560503775d3ebd25cb9d04d5c9e2172
Harris Shen <srh9572@gmail.com>
2023-01-24 21:39:59 -0500

    Organize table handler into separate file

--- app.js
+++ app.js
@@ -42,6 +42,8 @@ io.use((socket, next) => {
   next();
 });
 
+const registerTableHandler = require('./tableHandler');
+
 io.on('connection', (socket) => {
   const req = socket.request;
 
@@ -49,58 +51,7 @@ io.on('connection', (socket) => {
     console.log('Event:', event, args);
   });
 
-  socket.on('table:create', async (callback) => {
-    console.log('create table');
-    const tableID = await redisMng.createTable();
-    req.session.tableID = tableID;
-    socket.join(tableID);
-    console.log('table created: ' + tableID);
-    await redisMng.addPlayer(tableID, req.session.playerID);
-    callback({
-      tableID: tableID,
-      players: [req.session.playerID],
-      playerReady: [false],
-    });
-  });
-
-  socket.on('table:leave', async (callback) => {
-    console.log('leave table');
-    const tableID = req.session.tableID;
-    req.session.tableID = '';
-    await redisMng.removePlayer(tableID, req.session.playerID);
-    socket.leave(tableID);
-    const players = await redisMng.getPlayers(tableID);
-    console.log(players);
-    console.log('table:' + tableID + ' left');
-    io.to(tableID).emit('table:update', {players: players});
-    callback({tableID: req.session.tableID});
-  });
-
-  socket.on('table:join', async (tableID, callback) => {
-    console.log('joining table');
-    if(!req.session.playerID) {
-      req.session.playerID = 'User-' + nanoid(8);
-    }
-    try {
-      await redisMng.addPlayer(tableID, req.session.playerID);
-    } catch (error) {
-      console.log(error.message);
-      callback({error: error.message});
-      return;
-    }
-    req.session.tableID = tableID;
-    socket.join(tableID);
-    const players = await redisMng.getPlayers(tableID);
-    const playerReady = await redisMng.getPlayerReady(tableID);
-    console.log(players);
-    console.log('table:' + tableID + ' joined');
-    const data = {
-      players: players,
-      playerReady: playerReady,
-    }
-    io.to(tableID).emit('table:update', data);
-    callback(data);
-  });
+  registerTableHandler(io, socket, redisMng);
 
   socket.on('game:ready', async (callback) => {
     const tableID = req.session.tableID
---
+++ tableHandler.js
@@ -0,0 +1,56 @@
+module.exports = (io, socket, redisMng) => {
+  const req = socket.request;
+  
+  socket.on('table:create', async (callback) => {
+    console.log('create table');
+    const tableID = await redisMng.createTable();
+    req.session.tableID = tableID;
+    socket.join(tableID);
+    console.log('table created: ' + tableID);
+    await redisMng.addPlayer(tableID, req.session.playerID);
+    callback({
+      tableID: tableID,
+      players: [req.session.playerID],
+      playerReady: [false],
+    });
+  });
+
+  socket.on('table:leave', async (callback) => {
+    console.log('leave table');
+    const tableID = req.session.tableID;
+    req.session.tableID = '';
+    await redisMng.removePlayer(tableID, req.session.playerID);
+    socket.leave(tableID);
+    const players = await redisMng.getPlayers(tableID);
+    console.log(players);
+    console.log('table:' + tableID + ' left');
+    io.to(tableID).emit('table:update', {players: players});
+    callback({tableID: req.session.tableID});
+  });
+
+  socket.on('table:join', async (tableID, callback) => {
+    console.log('joining table');
+    if(!req.session.playerID) {
+      req.session.playerID = 'User-' + nanoid(8);
+    }
+    try {
+      await redisMng.addPlayer(tableID, req.session.playerID);
+    } catch (error) {
+      console.log(error.message);
+      callback({error: error.message});
+      return;
+    }
+    req.session.tableID = tableID;
+    socket.join(tableID);
+    const players = await redisMng.getPlayers(tableID);
+    const playerReady = await redisMng.getPlayerReady(tableID);
+    console.log(players);
+    console.log('table:' + tableID + ' joined');
+    const data = {
+      players: players,
+      playerReady: playerReady,
+    }
+    io.to(tableID).emit('table:update', data);
+    callback(data);
+  });
+}
