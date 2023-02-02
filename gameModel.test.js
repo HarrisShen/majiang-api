dc7b59a35911976d39604d7c2aff32f3910bbbe8
RH Shen <srh9572@gmail.com>
2023-02-02 15:26:47 -0500

    Add a test case

--- gameModel.test.js
+++ gameModel.test.js
@@ -10,6 +10,23 @@ function arrayEquals(a, b) {
     a.every((val, index) => val === b[index]);
 }
 
+test('No self Pong'), (t) => {
+  const tiles = [11, 11, 11];
+  const playerParams = [
+    [[12,12,12,13], [16,16,16,16], [15,15,15], 'no'],
+    [[19], [], [], 'no'],
+    [[21], [], [], 'no'],
+    [[31], [], [], 'no'],
+  ];
+  const players = [0, 1, 2, 3].map(i => new Player(...playerParams[i]));
+  const mjGame = new MahjongGame(tiles, players, 0, 1);
+  mjGame.checkActions();
+  assert(!mjGame.playerActions[0].pong);
+  mjGame.applyAction('discard', 0, 0);
+  assert(mjGame.status !== 2);
+  assert(!mjGame.playerActions[0].pong);
+}
+
 test('return Kong test', (t) => {
   const tiles = [11, 11, 11];
   const playerParams = [
